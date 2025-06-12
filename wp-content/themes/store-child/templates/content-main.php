f55ac41690fe598b8efdf39efa0f835a66c15b01
Evgeny <260443@mail.ru>
2025-06-12 12:53:56 +0500

    Description, manufacture, advantages sections

--- wp-content/themes/store-child/templates/content-main.php
+++ wp-content/themes/store-child/templates/content-main.php
@@ -72,7 +72,7 @@
       </div>
       <div class="personal-select-content">
         <div class="personal-select-content__title">Персональный подбор</div>
-        <div class="personal-select-content__description">Экспертная система подбора солей Шюсслера для тканевой биохимической терапии. Медицинский ассистент  для диагностики дефицитов минералов в организме человека, подбора персонифицированной комбинации с проверкой совместимости.</div>
+        <div class="personal-select-content__description">Экспертная система подбора солей Шюсслера для тканевой биохимической терапии. Медицинский ассистент для диагностики дефицитов минералов в организме человека, подбора персонифицированной комбинации с проверкой совместимости.</div>
         <div class="content-flex-container">
           <div class="example-btn">пример заключения</div>
           <div class="view-more-btn">подробнее</div>
@@ -95,177 +95,89 @@
 
 <div class="description-section section">
   <div class="container">
-    <div class="row">
-      <div class="col-xxl-3 col-md-4">
-        <div class="description-image">
-          <img src="/wp-content/themes/store-child/includes/images/shyussler.jpg" alt="">
-        </div>
+    <div class="flex-container">
+      <div class="description-image">
+        <img src="/wp-content/themes/store-child/includes/images/shyussler.jpg" alt="">
       </div>
-      <div class="col-xxl-9 col-md-8">
-        <div class="description-content">
-          <div class="title-wrapper">
-            <img src="/wp-content/themes/store-child/includes/images/ball-blue.png" class="image" alt="">
-            <div class="number">151</div>
-            <div class="text">год тканевой<br>биохимической<br>терапии</div>
-          </div>
-          <div class="description">
-            <p class="text">Тканевая биохимическая терапия разработана доктором Шюсслером в середине 19 века.</p>
-            <p class="text text-last">Основана на доказанной потребности организма в небольших дозах минеральных веществ, которые используются в качестве катализаторов химических реакций в клетках, строительного неорганического матрикса организма. Различные жизненные ситуации, эмоциональные переживания, нагрузки, заболевания приводят к дефициту отдельных минералов, в результате чего нарушаются биохимические процессы и развивается гипоксия тканей.</p>
-          </div>
-          <a href="/about-salts" class="view-more-btn">подробнее</a>
+      <div class="description-content">
+        <div class="description-content__title">Заголовок</div>
+        <div class="description-content__text">
+          <p>Тканевая биохимическая терапия разработана доктором Шюсслером в середине 19 века.</p>
+          <p>Основана на доказанной потребности организма в небольших дозах минеральных веществ, которые используются в качестве катализаторов химических реакций в клетках, строительного неорганического матрикса организма. Различные жизненные ситуации, эмоциональные переживания, нагрузки, заболевания приводят к дефициту отдельных минералов, в результате чего нарушаются биохимические процессы и развивается гипоксия тканей.</p>
         </div>
+        <div class="view-more-btn">подробнее</div>
       </div>
-    </div>    
+    </div>
   </div>
 </div>
 
 <div class="manufacture-section">
-  <div class="manufacture-title">
-    <div class="container">
-      <div class="title">Производство</div>
-      <div class="subtitle">made and owned</div>
-    </div>
-  </div>
-  <div class="manufacture-content">
+  <div class="manufacture-description">
     <div class="container">
       <div class="flex-container">
-        <div class="video">
-          <video controls poster="/wp-content/themes/store-child/includes/video/poster.jpg">
-            <source src="/wp-content/themes/store-child/includes/video/video.mp4" type="video/mp4">
-            Ваш браузер не поддерживает встроенные видео.
-          </video>
-        </div>
-
-        <div class="content">
-          <div class="description">
-            <p class="text">Тканевая биохимическая терапия разработана доктором Шюсслером в середине 19 века.</p>
-            <p class="text text-last">Основана на доказанной потребности организма в небольших дозах минеральных веществ, которые используются в качестве катализаторов химических реакций в клетках, строительного неорганического матрикса организма. Различные жизненные ситуации, эмоциональные переживания, нагрузки, заболевания приводят к дефициту отдельных минералов, в результате чего нарушаются биохимические процессы и развивается гипоксия тканей.</p>
+        <div class="manufacture-content">
+          <div class="manufacture-content__title">Изготовление персональных лекарств и натуропатии</div>
+          <div class="manufacture-content__text">
+            <p>Соли Шюсслера производятся в Европе на протяжении 150 лет.</p>
+            <p>В России производителем солей Шюсслера по оригинальной технологии является специализированная натуропатическая производственная аптека NaturaPharma.</p>
           </div>
-          <a href="/about-salts" class="view-more-btn">подробнее</a>
+          <div class="view-more-btn">подробнее</div>
+        </div>
+        <div class="manufacture-image">
+          <img src="/wp-content/themes/store-child/includes/images/manufacture-image.jpg" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="manufacture-image">
+    <div class="container">
+      <div class="grid-container">
+        <div class="image">
+          <img src="/wp-content/themes/store-child/includes/images/image1.jpg" alt="">
+        </div>
+        <div class="image">
+          <img src="/wp-content/themes/store-child/includes/images/image2.jpg" alt="">
+        </div>
+        <div class="image">
+          <img src="/wp-content/themes/store-child/includes/images/image3.jpg" alt="">
+        </div>
+        <div class="image">
+          <img src="/wp-content/themes/store-child/includes/images/image4.jpg" alt="">
         </div>
       </div>
     </div>
   </div>
 </div>
 
-<div class="image-section">
+<div class="advantages-section">
   <div class="container">
-    <div class="grid-container">
-      <div class="image">
-        <img src="/wp-content/themes/store-child/includes/images/image1.jpg" alt="">
-      </div>
-      <div class="image">
-        <img src="/wp-content/themes/store-child/includes/images/image2.jpg" alt="">
-      </div>
-      <div class="image">
-        <img src="/wp-content/themes/store-child/includes/images/image3.jpg" alt="">
-      </div>
-      <div class="image">
-        <img src="/wp-content/themes/store-child/includes/images/image4.jpg" alt="">
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="club-section">
-  <div class="club-title">
-    <div class="container">
-      <div class="title">Станьте участниками</div>
-      <div class="name">NaturaClub</div>
-      <div class="text">и пользуйтесь привилегиями</div>
-    </div>
-  </div>
-  <div class="club-content">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-3 col-sm-6">
-          <div class="club-item">
-            <div class="flex-container">
-              <div class="image">
-                <img src="/wp-content/themes/store-child/includes/images/percent-sale.png" alt="">
-              </div>
-              <div class="title">Скидки</div>
-            </div>
-            <div class="club-item__text">Участники получают до 20% скидки на предложения недели</div>
-            <div class="ball">
-              <img src="/wp-content/themes/store-child/includes/images/ball-darkblue.png" alt="">
-            </div>
-          </div>
+    <div class="advantages-section-title">Наши преимущества</div>
+    <div class="flex-container">
+      <div class="advantages-item">
+        <div class="advantages-item__image">
+          <img src="/wp-content/themes/store-child/includes/images/truck.png" alt="">
         </div>
-        <div class="col-lg-3 col-sm-6">
-          <div class="club-item">
-            <div class="flex-container">
-              <div class="image">
-                <img src="/wp-content/themes/store-child/includes/images/car-delivery.png" alt="">
-              </div>
-              <div class="title">Бесплатные<br>доставки</div>
-            </div>
-            <div class="club-item__text">Вы получаете бесплатную доставку по России при использовании сервиса подбора или покупке на сумму больше 10 000 руб.</div>
-            <div class="ball">
-              <img src="/wp-content/themes/store-child/includes/images/ball-burgundy.png" alt="">
-            </div>
-          </div>
+        <div class="advantages-item__title">Удобная доставка</div>
+      </div>
+      <div class="advantages-item">
+        <div class="advantages-item__image">
+          <img src="/wp-content/themes/store-child/includes/images/truck.png" alt="">
         </div>
-        <div class="col-lg-3 col-sm-6">
-          <div class="club-item">
-            <div class="flex-container">
-              <div class="image">
-                <img src="/wp-content/themes/store-child/includes/images/free-select.png" alt="">
-              </div>
-              <div class="title">Бесплатный<br>подбор</div>
-            </div>
-            <div class="club-item__text">Вы получаете бесплатный подбор персонального курса при покупке набора солей Шюсслера в деревянной коробке по 200 таб.</div>
-            <div class="ball">
-              <img src="/wp-content/themes/store-child/includes/images/ball-yellow.png" alt="">
-            </div>
-          </div>
+        <div class="advantages-item__title">Консультация специалистов</div>
+      </div>
+      <div class="advantages-item">
+        <div class="advantages-item__image">
+          <img src="/wp-content/themes/store-child/includes/images/truck.png" alt="">
         </div>
-        <div class="col-lg-3 col-sm-6">
-          <div class="club-item">
-            <div class="flex-container">
-              <div class="image">
-                <img src="/wp-content/themes/store-child/includes/images/chemicals.png" alt="">
-              </div>
-              <div class="title">Скидки на<br>персональные<br>лекарства</div>
-            </div>
-            <div class="club-item__text">Вы получаете купон на скидку для обслуживания в производственной аптеке NaturaPharma в каждом заказе</div>
-            <div class="ball">
-              <img src="/wp-content/themes/store-child/includes/images/ball-green.png" alt="">
-            </div>
-          </div>
-        </div>
+        <div class="advantages-item__title">Персональный подход</div>
       </div>
     </div>
-  </div>
-  <div class="club-form">
-    <div class="container">
-      <form id="club-form" class="form" method="post">
-        <div class="flex-container">
-          <div class="form-group">
-            <input type="text" name="name" id="club-name" class="input-field js-required-name" autocomplete="on">
-            <label for="club-name" class="label">имя<span class="terracota-color">*</span></label>
-          </div>
-          <div class="form-group">
-            <input type="text" id="club-surname" name="surname" class="input-field js-required-surname" autocomplete="on">
-            <label for="club-surname" class="label">фамилия<span class="terracota-color">*</span></label>
-          </div>
-          <div class="form-group">
-            <input type="text" id="club-phone" name="phone" class="input-field js-required-phone js-input-phone-mask" autocomplete="on">
-            <label for="club-phone" class="label">телефон<span class="terracota-color">*</span></label>
-          </div>
-          <div class="form-group">
-            <input type="email" id="club-email" name="email" class="input-field js-required-email" autocomplete="on">
-            <label for="club-email" class="label">эл. почта<span class="terracota-color">*</span></label>
-          </div>
-          <input type="hidden" name="message" value="Вступить в клуб">
-          <button id="club-submit-btn" type="button" class="club-submit-btn">вступить в клуб</button>
-        </div>
-        <div class="agreement-text">
-          <input type="checkbox" id="club-checkbox" name="checkbox" class="custom-checkbox js-required-checkbox" checked onchange="document.getElementById('club-submit-btn').disabled = !this.checked;">
-          <label for="club-checkbox" class="custom-checkbox-label"></label>
-          <span>Нажимая кнопку вы соглашаетесь с <a href="/politika-v-otnoshenii-obrabotki-personalnyh-dannyh">политикой обработки данных</a></span>
-        </div>
-      </form>
+    <div class="advantages-item">
+      <div class="advantages-item__image">
+          <img src="/wp-content/themes/store-child/includes/images/truck.png" alt="">
+      </div>
+      <div class="advantages-item__title">Высокий уровень качества</div>
+      <div class="advantages-item__text">Мы используем субстанции, только фармацевтического класса, каждый продукт проходит тестирование в лаборатории для обеспечения безопасности, чистоты и эффективности</div>
     </div>
   </div>
 </div>
