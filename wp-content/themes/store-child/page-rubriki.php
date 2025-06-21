3ca7aae4a4d467a70d0d86bd60fbb61a74c164d5
Evgeny <260443@mail.ru>
2025-06-21 21:34:44 +0500

    Rubriki page

---
+++ wp-content/themes/store-child/page-rubriki.php
@@ -0,0 +1,209 @@
+<?php
+/**
+ * About salts page
+ *
+ * @package WordPress
+ * @subpackage Store-Child Theme
+ * @since Store-Child Theme
+ */
+?>
+
+<?php get_header(); ?>
+  
+<div class="rubriki-page custom-page">
+  <div class="container">
+    <div class="page-title">Внутриаптечные прописи (рубрики)</div>
+    <div class="grid-container">
+      <div class="filter">
+        
+        <form class="filter" action="" method="get">
+          <div class="checkbox-group">
+            <div class="checkbox-group__title">Рубрика</div>
+            <label class="checkbox-label">
+              <span class="custom-checkbox-text">Все рубрики</span>
+              <input type="checkbox" name="name" class="checkbox" value="">
+              <span class="custom-checkbox"></span>
+            </label>
+            <label class="checkbox-label">
+              <span class="custom-checkbox-text">Детокс и дренаж</span>
+              <input type="checkbox" name="name" class="checkbox" value="">
+              <span class="custom-checkbox"></span>
+            </label>
+            <label class="checkbox-label">
+              <span class="custom-checkbox-text">Беременность</span>
+              <input type="checkbox" name="name" class="checkbox" value="">
+              <span class="custom-checkbox"></span>
+            </label>
+            <label class="checkbox-label">
+              <span class="custom-checkbox-text">Воспаление</span>
+              <input type="checkbox" name="name" class="checkbox" value="">
+              <span class="custom-checkbox"></span>
+            </label>
+            <label class="checkbox-label">
+              <span class="custom-checkbox-text">Детский возраст</span>
+              <input type="checkbox" name="name" class="checkbox" value="">
+              <span class="custom-checkbox"></span>
+            </label>
+            <label class="checkbox-label">
+              <span class="custom-checkbox-text">Детский возраст</span>
+              <input type="checkbox" name="name" class="checkbox" value="">
+              <span class="custom-checkbox"></span>
+            </label>
+            <label class="checkbox-label">
+              <span class="custom-checkbox-text">Диета и здоровый образ жизни</span>
+              <input type="checkbox" name="name" class="checkbox" value="">
+              <span class="custom-checkbox"></span>
+            </label>
+          </div>
+
+          <div class="checkbox-group">
+            <div class="checkbox-group__title">Проблемы</div>
+            <label class="checkbox-label">
+              <span class="custom-checkbox-text">Проблема 1</span>
+              <input type="checkbox" name="name" class="checkbox" value="">
+              <span class="custom-checkbox"></span>
+            </label>
+            <label class="checkbox-label">
+              <span class="custom-checkbox-text">Проблема 2</span>
+              <input type="checkbox" name="name" class="checkbox" value="">
+              <span class="custom-checkbox"></span>
+            </label>
+          </div>
+
+            <button type="submit" class="submit-btn primary-btn">Применить</button>
+            <a href="#" class="secondary-btn clear-btn">Очистить</a>
+          </form>
+
+
+
+      </div>
+      <div class="content">
+        <div class="products">
+          <div class="products-item">
+            <div class="image-wrapper">
+              <div class="products-item__image">
+                <img src="/wp-content/themes/store-child/includes/images/product-item-image.png" alt="">
+              </div>
+              <div class="products-item__icon">
+                <img src="/wp-content/themes/store-child/includes/images/product-item-icon.png" alt="">
+              </div>
+            </div>
+            <div class="products-item__title">Заголовок рубрики</div>
+            <ul class="list">
+              <li class="list-item">описание 1</li>
+              <li class="list-item">описание 1</li>
+              <li class="list-item">описание 1</li>
+              <li class="list-item">описание 1</li>
+            </ul>
+            <div class="see-more-btn-wrapper">
+              <a href="#" class="see-more-btn">Подробнее</a>
+            </div>
+          </div>
+          <div class="products-item">
+            <div class="image-wrapper">
+              <div class="products-item__image">
+                <img src="/wp-content/themes/store-child/includes/images/product-item-image.png" alt="">
+              </div>
+              <div class="products-item__icon">
+                <img src="/wp-content/themes/store-child/includes/images/product-item-icon.png" alt="">
+              </div>
+            </div>
+            <div class="products-item__title">Заголовок рубрики</div>
+            <ul class="list">
+              <li class="list-item">описание 1</li>
+              <li class="list-item">описание 1</li>
+              <li class="list-item">описание 1</li>
+              <li class="list-item">описание 1</li>
+            </ul>
+            <div class="see-more-btn-wrapper">
+              <a href="#" class="see-more-btn">Подробнее</a>
+            </div>
+          </div>
+          <div class="products-item">
+            <div class="image-wrapper">
+              <div class="products-item__image">
+                <img src="/wp-content/themes/store-child/includes/images/product-item-image.png" alt="">
+              </div>
+              <div class="products-item__icon">
+                <img src="/wp-content/themes/store-child/includes/images/product-item-icon.png" alt="">
+              </div>
+            </div>
+            <div class="products-item__title">Заголовок рубрики</div>
+            <ul class="list">
+              <li class="list-item">описание 1</li>
+              <li class="list-item">описание 1</li>
+              <li class="list-item">описание 1</li>
+              <li class="list-item">описание 1</li>
+            </ul>
+            <div class="see-more-btn-wrapper">
+              <a href="#" class="see-more-btn">Подробнее</a>
+            </div>
+          </div>
+          <div class="products-item">
+            <div class="image-wrapper">
+              <div class="products-item__image">
+                <img src="/wp-content/themes/store-child/includes/images/product-item-image.png" alt="">
+              </div>
+              <div class="products-item__icon">
+                <img src="/wp-content/themes/store-child/includes/images/product-item-icon.png" alt="">
+              </div>
+            </div>
+            <div class="products-item__title">Заголовок рубрики</div>
+            <ul class="list">
+              <li class="list-item">описание 1</li>
+              <li class="list-item">описание 1</li>
+              <li class="list-item">описание 1</li>
+              <li class="list-item">описание 1</li>
+            </ul>
+            <div class="see-more-btn-wrapper">
+              <a href="#" class="see-more-btn">Подробнее</a>
+            </div>
+          </div>
+          <div class="products-item">
+            <div class="image-wrapper">
+              <div class="products-item__image">
+                <img src="/wp-content/themes/store-child/includes/images/product-item-image.png" alt="">
+              </div>
+              <div class="products-item__icon">
+                <img src="/wp-content/themes/store-child/includes/images/product-item-icon.png" alt="">
+              </div>
+            </div>
+            <div class="products-item__title">Заголовок рубрики</div>
+            <ul class="list">
+              <li class="list-item">описание 1</li>
+              <li class="list-item">описание 1</li>
+              <li class="list-item">описание 1</li>
+              <li class="list-item">описание 1</li>
+            </ul>
+            <div class="see-more-btn-wrapper">
+              <a href="#" class="see-more-btn">Подробнее</a>
+            </div>
+          </div>
+          <div class="products-item">
+            <div class="image-wrapper">
+              <div class="products-item__image">
+                <img src="/wp-content/themes/store-child/includes/images/product-item-image.png" alt="">
+              </div>
+              <div class="products-item__icon">
+                <img src="/wp-content/themes/store-child/includes/images/product-item-icon.png" alt="">
+              </div>
+            </div>
+            <div class="products-item__title">Заголовок рубрики</div>
+            <ul class="list">
+              <li class="list-item">описание 1</li>
+              <li class="list-item">описание 1</li>
+              <li class="list-item">описание 1</li>
+              <li class="list-item">описание 1</li>
+            </ul>
+            <div class="see-more-btn-wrapper">
+              <a href="#" class="see-more-btn">Подробнее</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+<?php get_footer(); ?>
