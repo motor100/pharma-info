1d027207650196a7f015b7128c21a1cb7ae7bc98
Evgeny <260443@mail.ru>
2025-06-21 22:40:45 +0500

    Category html + css layout

--- wp-content/themes/store-child/woocommerce/archive-product.php
+++ wp-content/themes/store-child/woocommerce/archive-product.php
@@ -34,42 +34,47 @@ if (is_shop()) {
 } else { ?>
 
   <div class="archive-product single__prod">
-    <div class="catalog-inside">
-      <div class="catalog-section-title">Каталог</div>
-    </div>
-    
     <div class="container">
+      <div class="catalog-inside">
+        <div class="catalog-section-title"><?php echo woocommerce_page_title(); ?></div>
+      </div>
 
       <?php
+      /*
       $parentid = get_queried_object_id(); // id родительской категории
       $term_childs = get_term_children( $parentid, 'product_cat' ); // получить массив с id вложенных подкатегорий
+      */
       ?>
 
-      <?php if ($term_childs) {  ?>
+      <?php //if ($term_childs) {  ?>
 
         <?php
         // Вывод подкатегорий
+        /*
         $args = array(
           'parent' => $parentid, // id родительской категории
           'hide_empty' => true, // скрывать категории без товаров
           'order' => 'ASC'
         );
+        */
         ?>
 
-        <?php $subcats = get_terms( 'product_cat', $args ); ?>
+        <?php //$subcats = get_terms( 'product_cat', $args ); ?>
 
+        <!-- 
         <div class="subcategories">
           <div class="subcategories-item" onclick="location.reload()">
             <div class="subcategories-item__title">Все</div>
           </div>
-          <?php foreach($subcats as $subcat) { ?>
-            <div class="subcategories-item filter-btn" data-term-id="<?php echo $subcat->term_id; ?>">
-              <div class="subcategories-item__title"><?php echo $subcat->name; ?></div>
+          <?php //foreach($subcats as $subcat) { ?>
+            <div class="subcategories-item filter-btn" data-term-id="<?php //echo $subcat->term_id; ?>">
+              <div class="subcategories-item__title"><?php //echo $subcat->name; ?></div>
             </div>
-          <?php } ?>
+          <?php //} ?>
         </div>
+         -->
 
-      <?php } ?>
+      <?php //} ?>
         
       <?php
       if ( woocommerce_product_loop() ) {
@@ -117,15 +122,17 @@ if (is_shop()) {
     
     </div>
 
+    <!-- 
     <div id="to-top" class="to-top hidden-mobile">
       <div class="container">
         <div class="circle">
           <div class="image">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/includes/images/svg/arrow-top.svg" class="arrow-top" alt="">
+            <img src="<?php //echo get_stylesheet_directory_uri(); ?>/includes/images/svg/arrow-top.svg" class="arrow-top" alt="">
           </div>
         </div>
       </div>
     </div>
+     -->
 
   </div>
 <?php } ?>
